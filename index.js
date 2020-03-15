331712bf71776b2ae9700be51b7cf672e2b1e664
Sridevi Patchipulusu <sridevips@hotmail.com>
2020-03-14 23:39:36 -0500

    mongodb node driver

---
+++ index.js
@@ -0,0 +1,39 @@
+//mongodb node driver and communicate with mongodb server through it from a node application
+const MongoClient = require('mongodb').MongoClient; //mongodb nodejs driver imported from the mongoclient object
+const assert = require('assert').strict;
+
+const url = 'mongodb://localhost:27017/'; //mongodb server
+const dbname = 'nucampsite';
+
+//mongoclient connect method  connect to mangodb server
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+
+    assert.strictEqual(err, null);
+
+    console.log('Connected correctly to server');
+
+    const db = client.db(dbname); //this method will connect with nucampsite database on the mongodb server //db object interact with database
+
+    //delete = remove = drop //dropped collection
+    db.dropCollection('campsites', (err, result) => {
+        assert.strictEqual(err, null);
+        console.log('Dropped Collection', result);
+
+        const collection = db.collection('campsites');
+        
+        //insert document
+        collection.insertOne({name: "Breadcrumb Trail Campground", description: "Test"},
+        //call back function
+        (err, result) => {
+            assert.strictEqual(err, null);
+            console.log('Insert Document:', result.ops); //ops = contain of array
+
+            collection.find().toArray((err, docs) => {
+                assert.strictEqual(err, null); // check error
+                console.log('Found Documents:', docs);
+
+                client.close(); // immediately close the client connection to the mangodb server
+            });
+        });
+    });
+});
